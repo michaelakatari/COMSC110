5bdb72dfd75e2c4916eaf6a7bf9a3962d39b24dc
Michaela <michaela.katari@gmail.com>
2018-01-29 18:20:31 -0800

    initial code
     Changes to be committed:
    	new file:   Variable_Declarations_Exercise.cpp

---
+++ Variable_Declarations_Exercise.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int userAge;
+
+   cout << "Enter your age: ";
+   cin >> userAge;
+   cout << userAge << " is a great age." << endl;
+
+   return 0;
+}
